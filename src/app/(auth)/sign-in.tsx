d941273641c4c8c0ac1722154c3b7e7b3f453dbe
the-devbear <bjarne.haumann@gmail.com>
2025-02-08 12:32:36 +0100

    prettier

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -6,10 +6,10 @@ export default function SignInPage() {
   return (
     <View className='flex gap-4'>
       <Text>Sign-In Page</Text>
-      <Link href="/(app)" asChild>
+      <Link href='/(app)' asChild>
         <Text>Go to App</Text>
       </Link>
-      <Link href="/sign-up" asChild push>
+      <Link href='/sign-up' asChild push>
         <Text>Go to Sign Up</Text>
       </Link>
     </View>
--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -6,10 +6,10 @@ export default function SignUpPage() {
   return (
     <View className='flex gap-4'>
       <Text>Sign-Up Page</Text>
-      <Link href="/(app)" asChild>
+      <Link href='/(app)' asChild>
         <Text>Go to App</Text>
       </Link>
-      <Link href="/sign-in" asChild>
+      <Link href='/sign-in' asChild>
         <Text>Go to Sign In</Text>
       </Link>
     </View>
