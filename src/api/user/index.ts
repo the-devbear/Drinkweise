55f8833d3e63a1f8a9af9e35a5e22858960e8d16
the-devbear <bjarne.haumann@gmail.com>
2025-03-08 12:50:36 +0100

    DRINK-11 implement sign-up with password and user data retrieval

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,6 +1,7 @@
-import { AuthService } from './services/auth.service';
+import { supabase } from '@drinkweise/lib/supabase';
 
+import { AuthService } from './services/auth.service';
 export { UserModel } from './models/user.model';
 export { Gender, Genders } from './enums/gender';
 
-export const authService = new AuthService();
+export const authService = new AuthService(supabase);
