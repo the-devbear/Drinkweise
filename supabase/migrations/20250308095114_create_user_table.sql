8dbe25b8fb89e710302e503ff423b4e78d22c7c2
the-devbear <bjarne.haumann@gmail.com>
2025-03-08 10:54:57 +0100

    DRINK-11 create users table with authentication and row-level security and generate types

---
+++ supabase/migrations/20250308095114_create_user_table.sql
@@ -0,0 +1,99 @@
+create type "public"."gender" as enum ('male', 'female', 'other');
+
+create table "public"."users" (
+    "id" uuid not null,
+    "username" text not null default ''::text,
+    "profile_picture" text,
+    "height" smallint not null default '-1'::smallint,
+    "weight" numeric(5,2) not null default '-1'::numeric,
+    "gender" gender,
+    "has_completed_onboarding" boolean not null default false,
+    "created_at" timestamp with time zone not null default now(),
+    "updated_at" timestamp with time zone not null default now()
+);
+
+
+alter table "public"."users" enable row level security;
+
+CREATE UNIQUE INDEX users_pkey ON public.users USING btree (id);
+
+alter table "public"."users" add constraint "users_pkey" PRIMARY KEY using index "users_pkey";
+
+alter table "public"."users" add constraint "users_id_fkey" FOREIGN KEY (id) REFERENCES auth.users(id) ON UPDATE CASCADE ON DELETE CASCADE not valid;
+
+alter table "public"."users" validate constraint "users_id_fkey";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$begin
+insert into public.users (id, username)
+values (new.id, new.email);
+return new;
+end;$function$
+;
+
+grant delete on table "public"."users" to "anon";
+
+grant insert on table "public"."users" to "anon";
+
+grant references on table "public"."users" to "anon";
+
+grant select on table "public"."users" to "anon";
+
+grant trigger on table "public"."users" to "anon";
+
+grant truncate on table "public"."users" to "anon";
+
+grant update on table "public"."users" to "anon";
+
+grant delete on table "public"."users" to "authenticated";
+
+grant insert on table "public"."users" to "authenticated";
+
+grant references on table "public"."users" to "authenticated";
+
+grant select on table "public"."users" to "authenticated";
+
+grant trigger on table "public"."users" to "authenticated";
+
+grant truncate on table "public"."users" to "authenticated";
+
+grant update on table "public"."users" to "authenticated";
+
+grant delete on table "public"."users" to "service_role";
+
+grant insert on table "public"."users" to "service_role";
+
+grant references on table "public"."users" to "service_role";
+
+grant select on table "public"."users" to "service_role";
+
+grant trigger on table "public"."users" to "service_role";
+
+grant truncate on table "public"."users" to "service_role";
+
+grant update on table "public"."users" to "service_role";
+
+create policy "Enable select for users based on id"
+on "public"."users"
+as permissive
+for insert
+to public
+with check ((( SELECT auth.uid() AS uid) = id));
+
+
+create policy "Enable update for users based on id"
+on "public"."users"
+as permissive
+for update
+to public
+using ((( SELECT auth.uid() AS uid) = id))
+with check ((( SELECT auth.uid() AS uid) = id));
+
+
+
+
