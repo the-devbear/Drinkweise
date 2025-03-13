758d291941bb362232bdd95df3946fe76b37d62e
the-devbear <bjarne.haumann@gmail.com>
2025-03-13 23:02:14 +0100

    DRINK-6 update handle new user function to use the username from google and the profile picture

---
+++ supabase/migrations/20250313220033_update_handle_new_user_function.sql
@@ -0,0 +1,15 @@
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$begin
+insert into public.users (id, username,profile_picture)
+values (new.id, coalesce(nullif(new.raw_user_meta_data->>'name', ''), new.email),new.raw_user_meta_data->>'avatar_url');
+return new;
+end;$function$
+;
+
+
+
