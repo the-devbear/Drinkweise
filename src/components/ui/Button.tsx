2a54150fe016a024278bc10022e005ba5269e184
the-devbear <bjarne.haumann@gmail.com>
2025-03-16 17:30:29 +0100

    DRINK-6 fix button after new tsconfig

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -100,8 +100,8 @@ function convertToRGBA(rgb: string, opacity: number): string {
     throw new Error('Invalid RGB color format');
   }
   const red = parseInt(rgbValues[0], 10);
-  const green = parseInt(rgbValues[1], 10);
-  const blue = parseInt(rgbValues[2], 10);
+  const green = parseInt(rgbValues[1]!, 10);
+  const blue = parseInt(rgbValues[2]!, 10);
   if (opacity < 0 || opacity > 1) {
     throw new Error('Opacity must be a number between 0 and 1');
   }
