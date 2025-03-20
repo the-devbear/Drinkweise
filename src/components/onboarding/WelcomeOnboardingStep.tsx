734bfb3133da3039180d4ddb5c26e2d6a727c4aa
the-devbear <bjarne.haumann@gmail.com>
2025-03-20 13:30:34 +0100

    DRINK-14 add some animations

--- src/components/onboarding/WelcomeOnboardingStep.tsx
+++ src/components/onboarding/WelcomeOnboardingStep.tsx
@@ -1,14 +1,41 @@
-import { Text } from '@drinkweise/ui/Text';
+import { useColorScheme } from '@drinkweise/lib/useColorScheme';
+import { AnimatedText } from '@drinkweise/ui/Text';
 import { TextInput } from '@drinkweise/ui/TextInput';
+import { Ionicons } from '@expo/vector-icons';
 import { View } from 'react-native';
+import Animated, { FadeIn, FadeInDown } from 'react-native-reanimated';
 
 export interface WelcomeOnboardingStepProps {}
 
 export function WelcomeOnboardingStep({}: WelcomeOnboardingStepProps) {
+  const { colors } = useColorScheme();
+
   return (
-    <View className='flex flex-1 items-center  justify-center p-5'>
-      <Text variant='largeTitle'>Welcome!</Text>
-      <TextInput />
+    <View className='flex-1 p-5'>
+      <Animated.View entering={FadeIn.delay(300)} className='mb-5 mt-10 items-center'>
+        <Ionicons size={64} name='beer-outline' color={colors.primary} />
+      </Animated.View>
+      <View className='flex-1'>
+        <AnimatedText
+          entering={FadeInDown.delay(600)}
+          variant='title1'
+          className='mb-4 text-center font-bold'>
+          Welcome to Drinkweise!
+        </AnimatedText>
+        <AnimatedText
+          entering={FadeInDown.delay(900)}
+          className='text-center text-lg text-gray-600 dark:text-gray-400'>
+          Track your drinks, understand your habits, and maintain a healthy relationship with
+          alcohol.
+        </AnimatedText>
+        <Animated.View entering={FadeInDown.delay(1200)} className='flex-1 p-6'>
+          <TextInput
+            label='What should we call you?'
+            labelClassName='mb-2 text-lg font-semibold text-gray-900'
+            placeholder='Enter a username'
+          />
+        </Animated.View>
+      </View>
     </View>
   );
 }
