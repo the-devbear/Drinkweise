acf18bd51c0dac74acfb06734465f9855de93574
the-devbear <bjarne.haumann@gmail.com>
2025-03-26 13:38:11 +0100

    DRINK-15 clean up onboarding form

--- src/lib/forms/onboarding.ts
+++ src/lib/forms/onboarding.ts
@@ -15,26 +15,28 @@ export type OnboardingStep = keyof typeof ONBOARDING_STEPS;
 export const onboardingSchema = z.object({
   username: z
     .string({ required_error: 'Username can not be empty' })
-    .min(3, { message: 'Username must be at least 3 characters long' })
-    .max(20, { message: 'Username must be at most 20 characters long' })
-    .regex(/^[a-zA-Z0-9_'-]*$/, {
-      message: 'Username can only contain letters, numbers, and underscores',
-    }),
+    .min(3, 'Username must be at least 3 characters long')
+    .max(20, 'Username must be at most 20 characters long')
+    .regex(/^[a-zA-Z0-9_'-]*$/, 'Username can only contain letters, numbers, and underscores'),
   height: z
     .number({
       required_error: 'A height between 80 and 250cm must be provided',
       invalid_type_error: 'Height must be a number',
     })
-    .int({ message: 'Height must be a whole number' })
-    .min(80, { message: 'Height must be at least 80' })
-    .max(250, { message: 'Height must be at most 250' }),
+    .int('Height must be a whole number')
+    .min(80, 'Height must be at least 80')
+    .max(250, 'Height must be at most 250'),
   weight: z
     .number({
       required_error: 'A weight between 30 and 250kg must be provided',
       invalid_type_error: 'Weight must be a number',
     })
-    .min(30, { message: 'Weight must be at least 30' })
-    .max(250, { message: 'Weight must be at most 250' }),
+    .min(30, 'Weight must be at least 30')
+    .max(250, 'Weight must be at most 250')
+    .refine(
+      (value) => (value.toString().split('.')[1]?.length ?? 0) <= 2,
+      'Weight must have at most two decimal places'
+    ),
   gender: z.custom<UserModel['gender']>(),
 });
 
@@ -45,6 +47,9 @@ export function useOnboardingForm() {
   return useForm({
     defaultValues: {
       username: user?.username.includes('@') ? '' : (user?.username ?? ''),
+      height: user?.height === -1 ? undefined : user?.height,
+      weight: user?.weight === -1 ? undefined : user?.weight,
+      gender: user?.gender,
     },
     shouldFocusError: false,
     mode: 'onChange',
