d75c79a60beccf327ce4126e927fab7516dbfe2c
the-devbear <bjarne.haumann@gmail.com>
2025-03-27 08:36:01 +0100

    DRINK-16 create user service for handeling onboardin completion

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,5 +1,7 @@
 import { supabase } from '@drinkweise/lib/supabase';
 
 import { AuthService } from './services/auth.service';
+import { UserService } from './services/user.service';
 
 export const authService = new AuthService(supabase);
+export const userService = new UserService(supabase);
