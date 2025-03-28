d2ebe5bf6982fa4493eb794d16761b8693e39bf7
the-devbear <bjarne.haumann@gmail.com>
2025-03-28 23:53:22 +0100

    DRINK-18 add button to get back to other layout

--- src/app/(app)/drinks/session/index.tsx
+++ src/app/(app)/drinks/session/index.tsx
@@ -1,10 +1,19 @@
+import { Button } from '@drinkweise/components/ui/Button';
 import { Text } from '@drinkweise/components/ui/Text';
+import { useRouter } from 'expo-router';
 import { View } from 'react-native';
 
 export default function SessionPage() {
+  const router = useRouter();
   return (
     <View>
       <Text>This is the session starting page</Text>
+      <Button
+        onPress={() => {
+          router.replace('/drinks');
+        }}>
+        <Text>Go back to drinks</Text>
+      </Button>
     </View>
   );
 }
