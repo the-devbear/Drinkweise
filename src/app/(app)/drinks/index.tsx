6bd8e24987eaab2e837b783512031b8ef72b9ce0
the-devbear <bjarne.haumann@gmail.com>
2025-03-28 23:52:52 +0100

    DRINK-18 add start session card to page

--- src/app/(app)/drinks/index.tsx
+++ src/app/(app)/drinks/index.tsx
@@ -1,15 +1,34 @@
 import { Button } from '@drinkweise/components/ui/Button';
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@drinkweise/components/ui/Card';
 import { Text } from '@drinkweise/components/ui/Text';
 import { useRouter } from 'expo-router';
+import { ScrollView, TouchableOpacity } from 'react-native';
 
 export default function DrinksPage() {
   const router = useRouter();
   return (
-    <Button
-      onPress={() => {
-        router.replace('/drinks/session');
-      }}>
-      <Text>Start session</Text>
-    </Button>
+    <ScrollView className='p-3'>
+      <TouchableOpacity onPress={() => router.replace('/drinks/session')}>
+        <Card>
+          <CardHeader>
+            <CardTitle className='text-xl'>Open your Tab!</CardTitle>
+            <CardDescription className='text-base'>
+              Keep track of your drinks and enjoy responsibly.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className='gap-4'>
+            <Button onPress={() => router.replace('/drinks/session')}>
+              <Text>Begin Session</Text>
+            </Button>
+          </CardContent>
+        </Card>
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
