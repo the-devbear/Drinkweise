e4ae1b823094d5cab2ee595482cf7c78fd23da5e
the-devbear <bjarne.haumann@gmail.com>
2025-04-09 23:02:56 +0200

    DRINK-34 display an icon when the start time and the end time are not the same day

--- src/components/session/ConsumptionItem.tsx
+++ src/components/session/ConsumptionItem.tsx
@@ -1,9 +1,11 @@
 import { IntegerInput } from '@drinkweise/components/ui/IntegerInput';
 import { Text } from '@drinkweise/components/ui/Text';
 import { cn } from '@drinkweise/lib/cn';
+import { isSameDay } from '@drinkweise/lib/utils/date/is-same-day';
 import { useAppDispatch } from '@drinkweise/store';
 import { updateConsumptionAction } from '@drinkweise/store/drink-session';
 import type { DrinkConsumptionModel } from '@drinkweise/store/drink-session/models/consumption.model';
+import { Ionicons } from '@expo/vector-icons';
 import { TouchableOpacity, View } from 'react-native';
 
 import { TimePicker } from './TimePicker';
@@ -72,29 +74,37 @@ export function ConsumptionItem({
       </View>
       <View className='flex-[2] items-center'>
         {consumption.endTime ? (
-          <TimePicker
-            value={new Date(consumption.endTime)}
-            onChange={({ type, nativeEvent: { timestamp } }) => {
-              if (type !== 'set') {
-                return;
-              }
+          <View>
+            {!isSameDay(consumption.startTime, consumption.endTime) && (
+              <View className='absolute -top-1 right-0 z-10 flex-row items-center'>
+                <Ionicons name='calendar-clear-outline' className='text-xs text-foreground' />
+                <Text className='text-xs'>+1</Text>
+              </View>
+            )}
+            <TimePicker
+              value={new Date(consumption.endTime)}
+              onChange={({ type, nativeEvent: { timestamp } }) => {
+                if (type !== 'set') {
+                  return;
+                }
 
-              // TODO: Think of a better way to handle this
-              if (timestamp < consumption.startTime) {
-                return;
-              }
+                // TODO: Think of a better way to handle this
+                if (timestamp < consumption.startTime) {
+                  return;
+                }
 
-              dispatch(
-                updateConsumptionAction({
-                  drinkId,
-                  consumptionIndex: index,
-                  updatedConsumption: {
-                    endTime: timestamp,
-                  },
-                })
-              );
-            }}
-          />
+                dispatch(
+                  updateConsumptionAction({
+                    drinkId,
+                    consumptionIndex: index,
+                    updatedConsumption: {
+                      endTime: timestamp,
+                    },
+                  })
+                );
+              }}
+            />
+          </View>
         ) : (
           <TouchableOpacity
             className='h-6 w-6 items-center justify-center rounded-md border border-primary'
