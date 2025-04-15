3548f7763e07627d5c8f767f725aaf94edcd1371
the-devbear <bjarne.haumann@gmail.com>
2025-04-15 22:45:30 +0200

    DRINK-21 create complete page

---
+++ src/app/(app)/drinks/session/complete.tsx
@@ -0,0 +1,10 @@
+import { Text } from '@drinkweise/components/ui/Text';
+import { ScrollView } from 'react-native';
+
+export default function CompleteDrinkSessionPage() {
+  return (
+    <ScrollView>
+      <Text>TODO: Here is going to happen something big</Text>
+    </ScrollView>
+  );
+}
