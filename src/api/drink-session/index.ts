94e87b645a31bc726b0798c1f1ecc38266645a5b
the-devbear <bjarne.haumann@gmail.com>
2025-04-20 14:11:19 +0200

    DRINK-21 implement complet drink session for service

--- src/api/drink-session/index.ts
+++ src/api/drink-session/index.ts
@@ -1,6 +1,5 @@
 import { supabase } from '@drinkweise/lib/supabase';
 
-import type { IDrinkSessionService } from './interfaces/drink-session.service-api';
 import { DrinkSessionService } from './services/drink-session.service';
 
-export const drinkSessionService: IDrinkSessionService = new DrinkSessionService(supabase);
+export const drinkSessionService = new DrinkSessionService(supabase);
--- src/api/drink-session/services/drink-session.service.ts
+++ src/api/drink-session/services/drink-session.service.ts
@@ -1,5 +1,6 @@
 import type { Result } from '@drinkweise/lib/types/result.types';
 import type { TypedSupabaseClient } from '@drinkweise/lib/types/supabase.types';
+import type { PostgrestError } from '@supabase/supabase-js';
 
 import type { IDrinkSessionService } from '../interfaces/drink-session.service-api';
 import type { CompleteDrinkSessionRequestModel } from '../models/complete-drink-session-request.model';
@@ -7,8 +8,27 @@ import type { CompleteDrinkSessionRequestModel } from '../models/complete-drink-
 export class DrinkSessionService implements IDrinkSessionService {
   constructor(private readonly supabase: TypedSupabaseClient) {}
 
-  public async completeDrinkSession(drinkSession: CompleteDrinkSessionRequestModel): Result<true> {
-    console.log('DrinkSessionService:completeDrinkSession', drinkSession);
+  public async completeDrinkSession(
+    drinkSession: CompleteDrinkSessionRequestModel
+  ): Result<true, PostgrestError> {
+    const { error } = await this.supabase.rpc('complete_drink_session', {
+      name: drinkSession.name,
+      note: drinkSession.note,
+      start_time: drinkSession.startTime.toISOString(),
+      end_time: drinkSession.endTime.toISOString(),
+      consumptions: drinkSession.consumptions.map((consumption) => ({
+        drink_id: consumption.drinkId,
+        volume: consumption.volume,
+        start_time: consumption.startTime.toISOString(),
+        end_time: consumption.endTime.toISOString(),
+      })),
+    });
+
+    if (error) {
+      console.error('Error completing drink session:', error);
+      return { error };
+    }
+
     return { value: true };
   }
 }
