0bbfc29b49f055f97cee38a555d0cd5ce25ac88d
the-devbear <bjarne.haumann@gmail.com>
2025-04-23 10:53:35 +0200

    DRINK-21 add form to completion screen

--- src/app/(app)/drinks/session/complete.tsx
+++ src/app/(app)/drinks/session/complete.tsx
@@ -7,12 +7,27 @@ import { calculateTotalGrammsOfAlcoholConsumed } from '@drinkweise/lib/drink-ses
 import { dateFormatterWithoutYear } from '@drinkweise/lib/utils/date/date-formatters';
 import { roundedNumberFormatter } from '@drinkweise/lib/utils/number/number-formatters';
 import { useAppDispatch, useAppSelector } from '@drinkweise/store';
-import { activeDrinkSessionSelector } from '@drinkweise/store/drink-session';
+import {
+  activeDrinkSessionSelector,
+  updateSessionNameAction,
+  updateSessionNoteAction,
+} from '@drinkweise/store/drink-session';
 import { completeDrinkSessionAction } from '@drinkweise/store/drink-session/actions/complete-drink-session.action';
 import { userWeightSelector } from '@drinkweise/store/user';
-import { Redirect } from 'expo-router';
-import { useMemo } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Redirect, useFocusEffect } from 'expo-router';
+import { useCallback, useMemo } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { Alert, ScrollView, TouchableOpacity, View } from 'react-native';
+import { z } from 'zod';
+
+const completeDrinkSessionSchema = z.object({
+  name: z
+    .string({ required_error: 'Name is required' })
+    .min(1, 'Name is required')
+    .max(255, 'Name is too long, max 255 characters'),
+  note: z.string().max(500, 'Note is too long, max 500 characters').optional(),
+});
 
 export default function CompleteDrinkSessionPage() {
   const drinkSession = useAppSelector(activeDrinkSessionSelector);
@@ -27,6 +42,50 @@ export default function CompleteDrinkSessionPage() {
     [totalAlcoholConsumed, userWeight, drinkSession?.startTime]
   );
 
+  const {
+    control,
+    handleSubmit,
+    getValues,
+    formState: { isSubmitting, defaultValues, isValid, isSubmitted },
+  } = useForm({
+    defaultValues: {
+      name: drinkSession?.name === '' ? undefined : drinkSession?.name,
+      note: drinkSession?.note === '' ? undefined : drinkSession?.note,
+    },
+    resolver: zodResolver(completeDrinkSessionSchema),
+  });
+
+  useFocusEffect(
+    useCallback(() => {
+      return () => {
+        console.debug('Unsubscribing from focus effect');
+        const values = getValues();
+        if (values.name !== defaultValues?.name) {
+          dispatch(updateSessionNameAction({ name: values.name }));
+        }
+        if (values.note !== defaultValues?.note) {
+          dispatch(updateSessionNoteAction({ note: values.note ?? '' }));
+        }
+      };
+    }, [dispatch, getValues, defaultValues])
+  );
+
+  const onSubmit = handleSubmit(async (data) => {
+    const result = await dispatch(
+      completeDrinkSessionAction({
+        ...data,
+      })
+    );
+
+    if (completeDrinkSessionAction.rejected.match(result)) {
+      Alert.alert('Error', 'An error occurred while completing the session. Please try again.', [
+        {
+          text: 'OK',
+        },
+      ]);
+    }
+  });
+
   if (!drinkSession) {
     return <Redirect href='/drinks' />;
   }
@@ -34,11 +93,21 @@ export default function CompleteDrinkSessionPage() {
   return (
     <ScrollView className='flex-1 pt-5' keyboardDismissMode='on-drag'>
       <View className='px-3'>
-        <TextInput
-          size='lg'
-          inputClassName='font-semibold text-lg'
-          placeholder='Give your session a memorable name'
-          clearButtonMode='while-editing'
+        <Controller
+          control={control}
+          name='name'
+          render={({ field: { onChange, value, onBlur }, fieldState: { error } }) => (
+            <TextInput
+              size='lg'
+              inputClassName='font-semibold text-lg'
+              placeholder='Give your session a memorable name'
+              clearButtonMode='while-editing'
+              defaultValue={value}
+              onChangeText={onChange}
+              onBlur={onBlur}
+              errorMessage={error?.message}
+            />
+          )}
         />
         <View className='flex-row items-center gap-5 pt-3'>
           <TouchableOpacity
@@ -97,21 +166,37 @@ export default function CompleteDrinkSessionPage() {
             </Text>
           </TouchableOpacity>
         </View>
-        <TextInput
-          label='Note:'
-          multiline
-          className='mt-2'
-          containerClassName='h-auto'
-          inputClassName='min-h-24 text-start align-top text-[16px]'
-          placeholder='How was your session? Leave a note here...'
+        <Controller
+          control={control}
+          name='note'
+          render={({ field: { onChange, value, onBlur }, fieldState: { error } }) => (
+            <View>
+              <TextInput
+                label='Note:'
+                multiline
+                defaultValue={value}
+                className='mt-2'
+                containerClassName='h-auto'
+                inputClassName='min-h-24 text-start align-top text-[16px]'
+                placeholder='How was your session? Leave a note here...'
+                onChangeText={onChange}
+                onBlur={onBlur}
+                errorMessage={error?.message}
+              />
+              {error?.message === undefined && (
+                <Text className='mr-2 mt-1 self-end text-xs text-muted-foreground'>
+                  {value?.length ?? 0} / 500 characters
+                </Text>
+              )}
+            </View>
+          )}
         />
       </View>
       <Button
         className='mx-3 mt-4'
-        onPress={() => {
-          // TODO: For now fire and forget
-          dispatch(completeDrinkSessionAction({ session: drinkSession }));
-        }}>
+        disabled={!isValid && isSubmitted}
+        loading={isSubmitting}
+        onPress={onSubmit}>
         <Text>Complete</Text>
       </Button>
     </ScrollView>
