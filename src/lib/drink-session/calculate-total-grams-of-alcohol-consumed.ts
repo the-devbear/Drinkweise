0e531867b8ff584abdcf166fb4a0bb2e6b3c048d
the-devbear <bjarne.haumann@gmail.com>
2025-04-23 12:37:42 +0200

    DRINK-21 fix total alcohol grams calculation

--- src/lib/drink-session/calculate-total-grams-of-alcohol-consumed.ts
+++ src/lib/drink-session/calculate-total-grams-of-alcohol-consumed.ts
@@ -4,16 +4,13 @@ const ALCOHOL_DENSITY = 0.8;
 
 export function calculateTotalGramsOfAlcoholConsumed(drinks: DrinkModel[]): number {
   return drinks.reduce((previous, drink) => {
-    return (
-      (previous +
-        drink.consumptions.reduce((previousVolume, consumption) => {
-          if (consumption.endTime === undefined) {
-            return previousVolume;
-          }
-          return previousVolume + consumption.volume;
-        }, 0)) *
-      (drink.alcohol / 100) *
-      ALCOHOL_DENSITY
-    );
+    const totalDrinkVolume = drink.consumptions.reduce((previousVolume, consumption) => {
+      if (consumption.endTime === undefined) {
+        return previousVolume;
+      }
+      return previousVolume + consumption.volume;
+    }, 0);
+
+    return previous + totalDrinkVolume * (drink.alcohol / 100) * ALCOHOL_DENSITY;
   }, 0);
 }
