b68450d910e4b2477a0fc9cbc64500a2a55f9e72
the-devbear <bjarne.haumann@gmail.com>
2025-04-27 12:26:52 +0200

    DRINK-19 add search bar to add page

--- src/app/(app)/drinks/session/add.tsx
+++ src/app/(app)/drinks/session/add.tsx
@@ -1,5 +1,8 @@
 import { AddDrinkList } from '@drinkweise/components/session/add/AddDrinkList';
+import { TextInput } from '@drinkweise/components/ui/TextInput';
 import type { AddDrinkModel } from '@drinkweise/store/drink-session/models/add-drink.model';
+import { Ionicons } from '@expo/vector-icons';
+import { useState } from 'react';
 import { View } from 'react-native';
 
 // This is a mock data for the drinks
@@ -162,9 +165,25 @@ const DATA: AddDrinkModel[] = [
 ];
 
 export default function AddDrinkPage() {
+  const [search, setSearch] = useState('');
+
+  const filteredData = DATA.filter((drink) =>
+    drink.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
+
   return (
     <View className='flex-1'>
-      <AddDrinkList data={DATA} />
+      <TextInput
+        autoFocus
+        clearButtonMode='while-editing'
+        value={search}
+        className='bg-card px-4 py-2'
+        leftIcon={<Ionicons name='search' className='text-2xl leading-none text-foreground' />}
+        variant='card'
+        placeholder='Search...'
+        onChangeText={setSearch}
+      />
+      <AddDrinkList data={filteredData} />
     </View>
   );
 }
