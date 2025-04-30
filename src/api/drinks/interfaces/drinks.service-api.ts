e5eab01ba9a77922a1d8c59d20af40ee026b0e97
the-devbear <bjarne.haumann@gmail.com>
2025-04-30 19:59:27 +0200

    DRINK-19 add default page size

--- src/api/drinks/interfaces/drinks.service-api.ts
+++ src/api/drinks/interfaces/drinks.service-api.ts
@@ -2,6 +2,7 @@ import type { Result } from '@drinkweise/lib/types/result.types';
 import type { AddDrinkModel } from '@drinkweise/store/drink-session/models/add-drink.model';
 
 export interface IDrinkService {
+  DEFAULT_PAGE_SIZE: number;
   getPaginatedDrinks: (userId: string, cursor: string) => Result<AddDrinkModel[]>;
   searchDrinks: (userId: string, search: string) => Result<AddDrinkModel[]>;
 }
