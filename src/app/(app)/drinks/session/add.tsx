f81ba30bc549977b4d2b93696999e9499b2a43ec
the-devbear <bjarne.haumann@gmail.com>
2025-05-02 17:24:30 +0200

    DRINK-19 clean up code a bit

--- src/app/(app)/drinks/session/add.tsx
+++ src/app/(app)/drinks/session/add.tsx
@@ -1,6 +1,7 @@
+import { AddDrinkError } from '@drinkweise/components/session/add/AddDrinkError';
 import { AddDrinkListItem } from '@drinkweise/components/session/add/AddDrinkListItem';
+import { AddDrinkSkeletonItem } from '@drinkweise/components/session/add/AddDrinkSkeletonItem';
 import { ActivityIndicator } from '@drinkweise/components/ui/ActivityIndicator';
-import { Button } from '@drinkweise/components/ui/Button';
 import { Text } from '@drinkweise/components/ui/Text';
 import { TextInput } from '@drinkweise/components/ui/TextInput';
 import { useSearchDrinksQuery } from '@drinkweise/lib/drink-session/query/use-search-drinks-query';
@@ -29,6 +30,84 @@ export default function AddDrinkPage() {
     }
   }, [search, isSearchQueryActive, infiniteDrinksQuery, searchQuery]);
 
+  const renderListHeader = useCallback(() => {
+    if (searchQuery.isError && search.length > 0) {
+      return (
+        <View className='items-center justify-center py-3'>
+          <Ionicons name='sad-outline' className='text-5xl text-muted' />
+          <Text variant='title3' className='mt-2 text-center font-semibold'>
+            Something went wrong
+          </Text>
+          <Text variant='subhead' color='tertiary' className='mt-1 text-center'>
+            {searchQuery.error.message}
+          </Text>
+        </View>
+      );
+    }
+    return null;
+  }, [search.length, searchQuery.error?.message, searchQuery.isError]);
+
+  const renderListFooter = useCallback(() => {
+    if (infiniteDrinksQuery.isError && search.length === 0) {
+      return (
+        <AddDrinkError
+          message={infiniteDrinksQuery.error.message}
+          onRetry={() => {
+            infiniteDrinksQuery.fetchNextPage();
+          }}
+          isRetrying={infiniteDrinksQuery.isFetchingNextPage}
+          canRetry={infiniteDrinksQuery.errorUpdateCount < 2}
+        />
+      );
+    }
+
+    if (infiniteDrinksQuery.isFetchingNextPage) {
+      return <ActivityIndicator className='py-4' />;
+    }
+
+    if (!infiniteDrinksQuery.hasNextPage && drinks.length > 0) {
+      return (
+        <View className='py-4'>
+          <Text variant='footnote' className='text-center text-muted'>
+            No more drinks to load
+          </Text>
+        </View>
+      );
+    }
+    return null;
+  }, [drinks.length, infiniteDrinksQuery, search.length]);
+
+  const renderListEmpty = useCallback(() => {
+    if (infiniteDrinksQuery.isError || searchQuery.isError) {
+      return null;
+    }
+    if (infiniteDrinksQuery.isFetching) {
+      return Array.from({ length: 8 }, (_, index) => <AddDrinkSkeletonItem key={index} />);
+    }
+    if ((isSearchQueryActive && searchQuery.isLoading) || search !== debounceSearch) {
+      return <ActivityIndicator size='large' className='py-4' />;
+    }
+    return (
+      <View className='flex-1 items-center justify-center py-10'>
+        <Ionicons name='beer-outline' className='text-5xl text-muted' />
+        <Text variant='title3' className='mt-2 text-center font-semibold'>
+          No drinks found
+        </Text>
+        <Text variant='subhead' color='tertiary' className='mt-1 text-center'>
+          {search.length > 0 ? 'Try a different search term' : "Sorry we couldn't find any drinks"}
+        </Text>
+      </View>
+    );
+  }, [
+    debounceSearch,
+    infiniteDrinksQuery.isError,
+    infiniteDrinksQuery.isFetching,
+    isSearchQueryActive,
+    search,
+    searchQuery.isError,
+    searchQuery.isLoading,
+  ]);
+
   return (
     <View className='flex-1'>
       <TextInput
@@ -53,94 +132,10 @@ export default function AddDrinkPage() {
         onRefresh={onRefresh}
         refreshing={infiniteDrinksQuery.isRefetching || searchQuery.isRefetching}
         renderItem={({ item }) => <AddDrinkListItem drink={item} />}
-        ListHeaderComponent={() => {
-          if (searchQuery.isError && search.length > 0) {
-            return (
-              <View className='items-center justify-center py-3'>
-                <Ionicons name='sad-outline' className='text-5xl text-muted' />
-                <Text variant='title3' className='mt-2 text-center font-semibold'>
-                  Something went wrong
-                </Text>
-                <Text variant='subhead' color='tertiary' className='mt-1 text-center'>
-                  {searchQuery.error.message}
-                </Text>
-              </View>
-            );
-          }
-          return null;
-        }}
-        ListFooterComponent={() => {
-          if (infiniteDrinksQuery.isError && search.length === 0) {
-            return (
-              <View className='flex-1 items-center justify-center py-10'>
-                <Ionicons name='sad-outline' className='text-5xl text-muted' />
-                <Text variant='title3' className='mt-2 text-center font-semibold'>
-                  Something went wrong
-                </Text>
-                <Text variant='subhead' color='tertiary' className='mt-1 text-center'>
-                  {infiniteDrinksQuery.error.message}
-                </Text>
-                {infiniteDrinksQuery.errorUpdateCount < 2 ? (
-                  <Button
-                    loading={infiniteDrinksQuery.isFetchingNextPage}
-                    onPress={() => {
-                      infiniteDrinksQuery.fetchNextPage();
-                    }}
-                    className='mt-4'
-                    variant='primary'>
-                    <Text>Try again</Text>
-                  </Button>
-                ) : (
-                  <Text variant='subhead' color='tertiary' className='mt-1 text-center'>
-                    Please try again later
-                  </Text>
-                )}
-              </View>
-            );
-          }
-          if (infiniteDrinksQuery.isFetchingNextPage) {
-            return <ActivityIndicator className='py-4' />;
-          }
-
-          if (!infiniteDrinksQuery.hasNextPage && drinks.length > 0) {
-            return (
-              <View className='py-4'>
-                <Text variant='footnote' className='text-center text-muted'>
-                  No more drinks to load
-                </Text>
-              </View>
-            );
-          }
-          return null;
-        }}
-        ListEmptyComponent={() => {
-          if (infiniteDrinksQuery.isError || searchQuery.isError) {
-            return null;
-          }
-
-          if (infiniteDrinksQuery.isFetching) {
-            // TODO: Maybe add a skeleton here
-            return <ActivityIndicator size='large' className='py-4' />;
-          }
-
-          if ((isSearchQueryActive && searchQuery.isLoading) || search !== debounceSearch) {
-            return <ActivityIndicator size='large' className='py-4' />;
-          }
-
-          return (
-            <View className='flex-1 items-center justify-center py-10'>
-              <Ionicons name='beer-outline' className='text-5xl text-muted' />
-              <Text variant='title3' className='mt-2 text-center font-semibold'>
-                No drinks found
-              </Text>
-              <Text variant='subhead' color='tertiary' className='mt-1 text-center'>
-                {search.length > 0
-                  ? 'Try a different search term'
-                  : "Sorry we couldn't find any drinks"}
-              </Text>
-            </View>
-          );
-        }}
+        scrollEnabled={!infiniteDrinksQuery.isFetching}
+        ListHeaderComponent={renderListHeader}
+        ListFooterComponent={renderListFooter}
+        ListEmptyComponent={renderListEmpty}
         onEndReached={() => {
           if (search.length === 0 && !infiniteDrinksQuery.isError) {
             infiniteDrinksQuery.fetchNextPage();
