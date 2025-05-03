3eaf6497f16fd6e9cfb665b207b7d61f16e89431
the-devbear <bjarne.haumann@gmail.com>
2025-05-03 12:23:15 +0200

    DRINK-27 change supabase call with service

--- src/lib/sessions/query/use-infinite-sessions-query.ts
+++ src/lib/sessions/query/use-infinite-sessions-query.ts
@@ -1,5 +1,4 @@
-import { supabase } from '@drinkweise/lib/supabase';
-import { delay } from '@drinkweise/lib/utils/delay';
+import { drinkSessionService } from '@drinkweise/api/drink-session';
 import { useAppSelector } from '@drinkweise/store';
 import { userIdSelector } from '@drinkweise/store/user';
 import { useInfiniteQuery } from '@tanstack/react-query';
@@ -13,39 +12,20 @@ export function useInfiniteSessionsQuery() {
     queryKey: [SESSIONS_QUERY_KEY, userId],
     initialPageParam: '',
     queryFn: async ({ pageParam }) => {
-      let query = supabase
-        .from('drink_sessions')
-        .select('id, user_id, name, note, start_time, end_time, users(username)')
-        .eq('user_id', userId!)
-        .order('start_time', { ascending: false })
-        .limit(5);
-
-      if (pageParam) {
-        query = query.lt('start_time', pageParam);
-      }
-
-      await delay(3);
-
-      const { data, error } = await query;
+      const { value, error } = await drinkSessionService.getPaginatedDrinkSessionsByUserId(
+        userId!,
+        pageParam
+      );
 
       if (error) {
         throw error;
       }
 
-      if (!data) {
+      if (!value) {
         return [];
       }
 
-      const sessions = data.map((session) => ({
-        id: session.id,
-        name: session.name,
-        note: session.note,
-        startTime: session.start_time,
-        endTime: session.end_time,
-        userName: session.users.username,
-      }));
-
-      return sessions;
+      return value;
     },
     getNextPageParam: (lastPage) => {
       if (lastPage.length === 0) {
