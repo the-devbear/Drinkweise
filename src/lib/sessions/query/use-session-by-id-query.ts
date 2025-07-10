c51a58633e6fa435f0af72c286dd2d10c582676d
the-devbear <bjarne.haumann@gmail.com>
2025-07-10 17:49:11 +0200

    DRINK-27 remove delay

--- src/lib/sessions/query/use-session-by-id-query.ts
+++ src/lib/sessions/query/use-session-by-id-query.ts
@@ -1,5 +1,4 @@
 import { drinkSessionService } from '@drinkweise/api/drink-session';
-import { delay } from '@drinkweise/lib/utils/delay';
 import { SESSION_QUERY_KEY } from '@drinkweise/lib/utils/query/keys';
 import { useQuery } from '@tanstack/react-query';
 
@@ -7,7 +6,6 @@ export function useSessionByIdQuery(sessionId: string) {
   return useQuery({
     queryKey: [SESSION_QUERY_KEY, sessionId],
     queryFn: async ({ signal }) => {
-      await delay(2);
       const { value, error } = await drinkSessionService.getDrinkSessionById(sessionId, signal);
 
       if (error) {
